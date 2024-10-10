13aa518512e3297a527ed8a0b81f9722a654dcfb
Keiner28 <keinergodinez@gmail.com>
2024-10-09 18:05:43 -0600

    fix: :bug: update settings of middleware cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,11 +1,6 @@
 import cors from 'cors'
 
-const ACCEPTED_ORIGINS = [
-  'http://localhost:8080',
-  'http://localhost:3000',
-  'http://localhost:1234',
-  'https://movies.com'
-]
+const ACCEPTED_ORIGINS = ['https://client-api-rest-express.vercel.app', 'http://localhost:3000']
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
   cors({
